0800534c727401d61df829738f650b190983e607
VISS7289 <1526716943@qq.com>
2023-07-11 22:46:01 +0800

    尝试加武器系统

---
+++ Source/ActionRoguelike/Public/Actions/SAction_Fire.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actions/SAction.h"
+#include "SAction_Fire.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_API USAction_Fire : public USAction
+{
+	GENERATED_BODY()
+	
+};
